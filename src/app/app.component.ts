2ca7480294b19770c6d8797f7809669905941c86
= <=>
2018-12-17 14:46:18 +0100

    Moving post array from post-list to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  posts: Array<Object> = [
+    {
+      title: "Mon premier post",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quam necessitatibus itaque, provident illum quaerat mollitia porro, similique consectetur obcaecati laborum cumque! Laudantium aspernatur, cum aut dolorem hic minima dolor!",
+      loveIts: 0,
+      created_at: new Date()
+
+    },
+
+    {
+      title: "Mon deuxième message",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quam necessitatibus itaque, provident illum quaerat mollitia porro, similique consectetur obcaecati laborum cumque!Laudantium aspernatur, cum aut dolorem hic minima dolor!",
+      loveIts: 0,
+      created_at: new Date()
+
+    },
+
+    {
+      title: "Enfin, un troisième article",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quam necessitatibus itaque, provident illum quaerat mollitia porro, similique consectetur obcaecati laborum cumque!Laudantium aspernatur, cum aut dolorem hic minima dolor!",
+      loveIts: 0,
+      created_at: new Date()
+
+
+    },
+
+
+  ]
+
+
   title = 'blog';
 }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -7,34 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  posts: Array<Object> = [
-    {
-      title: "Mon premier post",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quam necessitatibus itaque, provident illum quaerat mollitia porro, similique consectetur obcaecati laborum cumque! Laudantium aspernatur, cum aut dolorem hic minima dolor!",
-      loveIts: 0,
-      created_at: new Date()
-
-    },
-
-    {
-      title: "Mon deuxième message",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quam necessitatibus itaque, provident illum quaerat mollitia porro, similique consectetur obcaecati laborum cumque!Laudantium aspernatur, cum aut dolorem hic minima dolor!",
-      loveIts: 0,
-      created_at: new Date()
-
-    },
-
-    {
-      title: "Enfin, un troisième article",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quam necessitatibus itaque, provident illum quaerat mollitia porro, similique consectetur obcaecati laborum cumque!Laudantium aspernatur, cum aut dolorem hic minima dolor!",
-      loveIts: 0,
-      created_at: new Date()
-
-
-    },
-
-
-  ]
+  @Input() posts;
 
   constructor() { }
 
